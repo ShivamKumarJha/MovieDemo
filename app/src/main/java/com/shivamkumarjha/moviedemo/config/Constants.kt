e5d87a5db66600d94f34a5d7956161dff2c285f1
ShivamKumarJha <jha.shivam3@gmail.com>
2021-09-08 12:16:04 +0530

    config
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/main/java/com/shivamkumarjha/moviedemo/config/Constants.kt
@@ -0,0 +1,20 @@
+package com.shivamkumarjha.moviedemo.config
+
+object Constants {
+
+    const val TAG = "Airlines"
+
+    //API
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val PAGE_SIZE = 10
+
+    //Store key in build.gradle or externally in local.properties ideally for security reasons
+    const val API_KEY = "d5b568462e39f02e011bb612583ead1e"
+    const val API_SORT_QUERY = "popularity.desc"
+
+    //Database
+    const val DB_NAME = "app_database.db"
+
+    //Navigation
+    const val NAV_MAIN = "NAV_MAIN"
+}
